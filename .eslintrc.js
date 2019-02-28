669c74c9f819df7f4dd7623c37884b9e2d20e65b
StefanoMagrassi <1291271+StefanoMagrassi@users.noreply.github.com>
2019-02-28 17:15:05 +0100

    eslint config in separated file

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: 'eslint:recommended',
+  env: {
+    node: true,
+    es6: true
+  },
+  rules: {
+    indent: ['error', 2],
+    quotes: ['error', 'single']
+  }
+};
