d1690b20750b70116129cf34230709352ee037ff
StefanoMagrassi <1291271+StefanoMagrassi@users.noreply.github.com>
2020-01-21 09:03:22 +0100

    fix tests

--- test/_config.es.js
+++ test/_config.es.js
@@ -1,3 +1,3 @@
 module.exports = {
-  extends: ['../dist/index.js']
+  extends: ['../index.js']
 };
--- test/_config.ts.js
+++ test/_config.ts.js
@@ -1,3 +1,3 @@
 module.exports = {
-  extends: ['../dist/typescript.js']
+  extends: ['../typescript.js']
 };
